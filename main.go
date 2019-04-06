bb8d9cc6634f43c4b2ac3cd227e2b3daccb34270
leslie-alldridge <leslie.alldridge@gmail.com>
2019-04-06 21:59:21 +1300

    getting api

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 	fmt.Print("running")
+
+	// get api
+	response, err := http.Get("https://httpbin.org/ip")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
 }
